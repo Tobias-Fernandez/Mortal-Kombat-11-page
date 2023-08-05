c21b90e7a254330d5bb36b7057e0d019caf9c1f0
Tobias Fernandez <draxsonic333@gmail.com>
2023-08-05 15:56:52 -0300

    agregado

--- logica.js
+++ logica.js
@@ -1,22 +1,25 @@
-let inputHome = document.getElementsByClassName("input-home");
 let boton = document.getElementsByClassName("boton-home-icon");
- 
+
 boton.onclick = () => {
-    if (inputHome.value = personajes.find(personaje => personaje.nombre === pj.toLowerCase()) != undefined) {
-        document.getElementById("body-home").innerHTML = `
-            <body>
-                ${personajes.link}
-            </body>
-        `;
+    encontrarPj();
+}
+
+function encontrarPj() {
+    let nombre = document.getElementsByClassName("input-home").value;
+    let pjData = document.getElementById("data-pj");
+
+    let pjEncontrado = personajes.find(personaje => personaje.nombre === nombre.toLowerCase());
+
+    if (pjEncontrado) {
+        location.href = pjEncontrado.link;
+    } else {
+        pjData.textContent = "Nombre de personaje incorrecto";
     }
 }
 
-inputHome.onkeyup = () => {
-    if (inputHome.value = personajes.find(personaje => personaje.nombre === pj.toLowerCase()) != undefined) {
-        document.getElementById("body-home").innerHTML = `
-            <body>
-                ${personajes.link}
-            </body>
-        `;
-    }
-}
+/*if (personajes.find(personaje => personaje.nombre === inputHome.value.toLowerCase()) != undefined) {
+
+    const pj = personajes.find(personaje => personaje.nombre == inputHome.value.toLowerCase());
+
+    window.location.href = pj.link;
+}*/
